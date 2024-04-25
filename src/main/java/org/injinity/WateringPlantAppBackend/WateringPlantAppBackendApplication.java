f5459612e400bc9bb0bc1e9f76035151c25c60cf
jorchar <vixier61@gmail.com>
2024-04-25 22:19:55 +0200

    Initial commit

---
+++ src/main/java/org/injinity/WateringPlantAppBackend/WateringPlantAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.injinity.WateringPlantAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringPlantAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WateringPlantAppBackendApplication.class, args);
+	}
+
+}
